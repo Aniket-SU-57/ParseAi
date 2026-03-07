b049a6075329bdf4cb886ebda97908d063b50569
Aniket-SU-57 <araj07857@gmail.com>
2026-03-07 23:21:18 +0700

    api integration of  login with dagger hilt commit day 4

--- app/src/main/java/com/adityaproj/parseai/Authapis/Authentication.kt
+++ app/src/main/java/com/adityaproj/parseai/Authapis/Authentication.kt
@@ -2,15 +2,21 @@ package com.adityaproj.parseai.Authapis
 
 import com.adityaproj.parseai.dataauth.Signupresponse
 import com.adityaproj.parseai.dataauth.signupdata
-import com.adityaproj.parseai.models.StatusResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Path
+
 
 interface Authentication {
     @POST("/auth/signup/")
     suspend fun sign(
         @Body request: signupdata
     ): Response<Signupresponse>
+
+    @GET("/api/auth/login/")
+    suspend fun login(
+
+    )
 }
+
