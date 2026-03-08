3dac52ec90178dd8305d5c2a8d35867beb8fb76e
Aniket-SU-57 <araj07857@gmail.com>
2026-03-08 20:25:09 +0700

    api integration of  logut with dagger hilt . the logut part is cleared  commit day 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -50,6 +52,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.compose.ui:ui:1.3.0+")
 
     // Compose
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
